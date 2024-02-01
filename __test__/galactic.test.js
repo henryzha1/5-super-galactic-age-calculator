596c704079cd5f1b0664817ea76412aad0995c6d
Henry Zhang <henz51111@gmail.com>
2024-02-01 14:18:31 -0800

    red light for generating ages on other planets

--- __test__/galactic.test.js
+++ __test__/galactic.test.js
@@ -9,4 +9,10 @@ describe('Galactic', () => {
   test('should create Galactic object with earthAge property', () => {
     expect(galactic.earthAge).toEqual(10);
   });
+  test('should create Galactic object with age of other planets', () => {
+    expect(galactic.mercuryAge).toEqual(41.67);
+    expect(galactic.venusAge).toEqual(16.13);
+    expect(galactic.marsAge).toEqual(5.32);
+    expect(galactic.jupiteryAge).toEqual(0.84);
+  });
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,7 +1,14 @@
 export default class Galactic {
   constructor(earthAge) {
     this.earthAge = earthAge;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+    
   }
+
+
 }
 
 

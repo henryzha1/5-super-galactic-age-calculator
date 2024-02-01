9e74d31a7407f3dd4078cc947cf0982bda336b6d
Henry Zhang <henz51111@gmail.com>
2024-02-01 15:53:02 -0800

    green light

--- __test__/galactic.test.js
+++ __test__/galactic.test.js
@@ -1,4 +1,4 @@
-import Galactic from './../src/galactic.js';
+import { Galactic, yearsUntil } from './../src/galactic.js';
 
 describe('Galactic', () => {
   let galactic;
@@ -15,8 +15,11 @@ describe('Galactic', () => {
     expect(galactic.marsAge).toEqual(5.32);
     expect(galactic.jupiterAge).toEqual(0.84);
   });
+  // test('should return 0 given same year in years from function', () => {
+  //   expect(galactic.yearsUntil(10)).toEqual();
+  // });
   test('should return an array of years until given year on earth and different planets with index 0 being 0 for past date and 1 for future date', () => {
-    expect(galactic.yearsUntil(15)).toEqual([1,5,20.83,8.06,2.66,0.42]);
-    expect(galactic.yearsUntil(5)).toEqual([0,5,20.83,8.06,2.66,0.42]);
+    expect(yearsUntil(galactic, 15)).toEqual([1,5,20.83,8.06,2.66,0.42]);
+    expect(yearsUntil(galactic, 5)).toEqual([0,5,20.83,8.06,2.66,0.42]);
   });
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,16 +1,24 @@
-export default class Galactic {
+export class Galactic {
   constructor(earthAge) {
     this.earthAge = earthAge;
     this.mercuryAge = parseFloat((earthAge/0.24).toFixed(2));
     this.venusAge = parseFloat((earthAge/0.62).toFixed(2));
     this.marsAge = parseFloat((earthAge/1.88).toFixed(2));
     this.jupiterAge = parseFloat((earthAge/11.86).toFixed(2));
-
   }
 
-  yearsUntil(year) {
-    return 0;
-  }
+  
 }
 
-
+export function yearsUntil(galactic, year) {
+  let startingIndex = -1;
+  let difference = galactic.earthAge - year;
+  if(difference < 0) {
+    startingIndex = 1;
+    difference = Math.abs(difference);
+  } else {
+    startingIndex = 0;
+  }
+  let galacticDifference = new Galactic(difference);
+  return [startingIndex, galacticDifference.earthAge, galacticDifference.mercuryAge, galacticDifference.venusAge, galacticDifference.marsAge, galacticDifference.jupiterAge];
+}

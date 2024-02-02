255a5abf09911f9f1d089c396663fea21ba404c9
Henry Zhang <henz51111@gmail.com>
2024-02-01 23:52:24 -0800

    green light

--- src/further-exploration.js
+++ src/further-exploration.js
@@ -9,5 +9,8 @@ export function daysUntilNextBirthday(birthday, day) {
 }
 
 export function dogAgeOnJupiterIn2073(birthday) {
-  return 0;
+  let day = new Date("2073-01-01");
+
+  let daysUntil = new Galactic((daysUntilNextBirthday(birthday,day).earth/365).toFixed(2)*7);
+  return daysUntil.jupiter;
 }

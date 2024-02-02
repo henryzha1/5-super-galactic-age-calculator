81a1bd71435ea4a4852abdc238c454ddd8674647
Henry Zhang <henz51111@gmail.com>
2024-02-01 23:42:22 -0800

    green light

--- src/further-exploration.js
+++ src/further-exploration.js
@@ -1,5 +1,9 @@
 import { Galactic } from './galactic.js';
 
 export function daysUntilNextBirthday(birthday, day) {
-  return birthday + day;
+  let dayOfBirthday = birthday.getTime()/1000/60/60/24;
+  let dayOfDay = day.getTime()/1000/60/60/24;
+
+  let galactic = new Galactic(dayOfBirthday,dayOfDay);
+  return galactic.difference;
 }
